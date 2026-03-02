d409c4a72cb3948d95cfab89c32eca1f70bf774f
Raul <raul.novais@edu.senai.br>
2026-03-02 17:08:29 -0300

    ADD criei o jogo med libs

---
+++ Jogo teca.py
@@ -0,0 +1,32 @@
+print(""" 
+███    ███  █████  ██████      ██      ██ ██████  ███████ 
+████  ████ ██   ██ ██   ██     ██      ██ ██   ██ ██      
+██ ████ ██ ███████ ██   ██     ██      ██ ██████  ███████ 
+██  ██  ██ ██   ██ ██   ██     ██      ██ ██   ██      ██ 
+██      ██ ██   ██ ██████      ███████ ██ ██████  ███████ 
+                                                          
+                                                          """)
+
+animal=input ("digite um animal: ")
+adjetivo=input("digite um adjetivo:")
+verbo=input("digite um verbo: ")
+objeto=input("digite um objeto")
+lugar=input("digite um luguar")
+emocao=input("digite um emoçao")
+print(f"""Era uma noite fria no {lugar}. A lua iluminava as lápides antigas, e o vento fazia as árvores sussurrarem como se guardassem segredos.
+
+De repente, comecei a {verbo} um miado estranho. Não era um miado comum — era longo, arrastado, quase humano. Meu coração acelerou.
+
+Entre as sombras apareceu {adjetivo}, com o pelo todo desgrenhado e os olhos brilhando de um jeito assustador. Ele não parecia um {animal} qualquer. Havia algo errado. Muito errado.
+
+O {animal} estava parado ao lado de uma velha lápide, e ao seu lado havia uma arma enferrujada fincada na terra. Como aquilo foi parar ali? Quem teria deixado?
+
+O silêncio ficou pesado. Eu sentia um enorme {emocao}, como se algo estivesse prestes a acontecer. O {animal} me encarava sem piscar. Então, lentamente, ele abriu a boca — mas, em vez de miar, uma voz sussurrou:
+
+— Você também consegue ouvir…
+
+O vento soprou forte. A faca caiu no chão.
+
+E o {animal} desapareceu.
+
+Até hoje, quando passo perto {lugar}, ainda consigo ouvir o mesmo miado ecoando na escuridão.""")
