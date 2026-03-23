593a2dddeac366aa2545ed2ac0079890a70961b3
Raul <raul.novais@edu.senai.br>
2026-03-23 14:53:45 -0300

    end e or

---
+++ 02_adivinha_numero/Adivinhaçao.py
@@ -0,0 +1,11 @@
+import random
+
+numero_aleatorio = random.randint(0,20)
+
+numero_escolhido = int (input("escolha um numero:"))
+
+if numero_escolhido==numero_aleatorio:
+   print("você é dimais! Parabéns!!!")
+else:
+    print("vc é fraco")
+    print (f"o numero era : {numero_aleatorio}")
