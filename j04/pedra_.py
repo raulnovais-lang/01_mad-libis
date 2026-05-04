5beecf509ddf6cc0d466d634a0ca7833eb49708d
Raul <raul.novais@edu.senai.br>
2026-05-04 15:00:39 -0300

    fiz menu

---
+++ j04/pedra_.py
@@ -0,0 +1,24 @@
+import random
+def jogar_pedra_papel_tesoura ():
+
+    print("""
+    *****************************************************************************************
+    ***                                Pedra                                              ***
+    ***                                Papel                                              ***
+    ***                                Tesoura                                            ***
+    *****************************************************************************************
+
+    """)
+    escolha= input("escolha um Pedra,papel,tesoura: ")
+    escolha_computador = random.choice(["pedra","papel","tesoura"])
+    print(f"o que eu escolhi foi {escolha_computador}")
+    if escolha== escolha_computador:
+        print("deu o empate")
+    elif escolha=="pedra" and escolha_computador=="tesoura":
+        print("vc ganhou")
+    elif escolha == "papel" and escolha_computador== "pedra":
+        print("vc ganhou...")
+    elif escolha == "tesoura" and escolha_computador== "papel":
+        print("vc ganhou...")
+    else:
+        print("vc perdeu")
