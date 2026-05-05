969d00ad29fb46416da7124f4f61d4f9a75f6521
raul <raul.novais@edu.senai.br>
2026-05-05 08:56:04 -0300

    incluiu novo item no menu

--- j05/maior_menor.py
+++ j05/maior_menor.py
@@ -1,19 +1,21 @@
+
 import random
-vidas=5
-numero_aleatorio = random.randint(0,100)
-while True:
-    numero_escolhido = int (input("escolha um numero:"))
+def jogar_maior_menor():
+    vidas=5
+    numero_aleatorio = random.randint(0,100)
+    while True:
+        numero_escolhido = int (input("escolha um numero:"))
 
-    if numero_escolhido==numero_aleatorio:
-        print("você é dimais! Parabéns!!!")
-        break
-    else:
-        if numero_escolhido > numero_aleatorio:
-            print("o numero e menor entao vc perdeu")
-        elif numero_escolhido < numero_aleatorio:
-            print("o numero e maior então vc perdeu")
-        vidas= vidas - 1
-
-        if vidas==0:
-            print("vc perdeu tudo, lindo! tchau obrigado! ")
+        if numero_escolhido==numero_aleatorio:
+            print("você é dimais! Parabéns!!!")
             break
+        else:
+            if numero_escolhido > numero_aleatorio:
+                print("o numero e menor entao vc perdeu")
+            elif numero_escolhido < numero_aleatorio:
+                print("o numero e maior então vc perdeu")
+            vidas= vidas - 1
+
+            if vidas==0:
+                print("vc perdeu tudo, lindo! tchau obrigado! ")
+                break
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from j02.Adivinhaçao import jogar_adivinha_numero
 from j01.madlibis import jogar_madlibis
 from j03.par_ou_impar import jogar_par_impar
 from j04.pedra_ import jogar_pedra_papel_tesoura
+from j05.maior_menor import jogar_maior_menor
 
 while 1==1:
     print(""" 
@@ -10,6 +11,7 @@ while 1==1:
     *       02- adinvinhação                  *
     *       03- par ou impar                  *
     *       04- pedra                         *
+    *       05- maior ou menor                *
     *       0- sair                           *
     ###########################################
     """)
@@ -26,6 +28,9 @@ while 1==1:
 
     elif escolha == 4:
         jogar_pedra_papel_tesoura ()
+
+    elif escolha == 5:
+        jogar_maior_menor ()
 
     elif escolha == 0:
         print("foi otimo jogar com vc")
