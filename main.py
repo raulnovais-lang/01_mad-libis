908ea8baf956cc5c2a14c906255564bb4eb1cb92
raul <raul.novais@edu.senai.br>
2026-05-05 08:44:22 -0300

    maior e menor

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ from j01.madlibis import jogar_madlibis
 from j03.par_ou_impar import jogar_par_impar
 from j04.pedra_ import jogar_pedra_papel_tesoura
 
-print(""" 
-###########################################
-*       01- main libs                     *
-*       02- adinvinhação                  *
-*       03- par ou impar                  *
-*       04- pedra                         *
-*       0- sair                           *
-###########################################
-""")
-escolha = int(input("por favor escolha o seu jogo:"))
+while 1==1:
+    print(""" 
+    ###########################################
+    *       01- main libs                     *
+    *       02- adinvinhação                  *
+    *       03- par ou impar                  *
+    *       04- pedra                         *
+    *       0- sair                           *
+    ###########################################
+    """)
+    escolha = int(input("por favor escolha o seu jogo:"))
 
-if escolha == 1:
-    jogar_madlibis ()
+    if escolha == 1:
+        jogar_madlibis ()
 
-elif escolha == 2:
-    jogar_adivinha_numero()
+    elif escolha == 2:
+        jogar_adivinha_numero()
 
-elif escolha == 3:
-    jogar_par_impar ()
+    elif escolha == 3:
+        jogar_par_impar ()
 
-elif escolha == 4:
-    jogar_pedra_papel_tesoura ()
+    elif escolha == 4:
+        jogar_pedra_papel_tesoura ()
 
-elif escolha == 0:
-    print("foi otimo jogar com vc")
-    break
+    elif escolha == 0:
+        print("foi otimo jogar com vc")
+        break
 
