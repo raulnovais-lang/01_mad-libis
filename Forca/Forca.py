8b6bfe3000f73dd7bcbd05b01f848732b60fea75
Raul <raul.novais@edu.senai.br>
2026-05-18 11:02:08 -0300

    forca2 _2

--- Forca/Forca.py
+++ Forca/Forca.py
@@ -18,8 +18,9 @@ def escolher_palavra() -> str:
 #palavra_escolida = escolher_palavra()
 
 #print(palavra_escolida)
-def desenhar_forca (erro):
+def desenhar_forca (erro:int):
     """imprima o desenho da forca denperdendo de quantidade de erros"""
+    limpar()
     if erro == 0:
         print(r"""
          _____          
@@ -89,3 +90,25 @@ def desenhar_forca (erro):
         |     |
         |    / \
         |       """)
+
+#desenhar_forca(5) - somente para testar se está correto
+
+def gerar_tracos(palavra: str) -> list:
+    """gere e retorna uma lista conteudos underlines (_) na mesma quantidade que as letras da palavra"""
+    quantidade_de_letras =len(palavra)
+    tracos = []    
+#usando while e o contador 
+    contador = 0
+    while contador < quantidade_de_letras:
+        tracos.append("_")
+        contador = contador + 1
+    
+    #while len (tracos) < quantidade_de_letras
+    #tracos.append("_")
+    
+    #for x in palavra:
+    #    tracos.append("_")
+    return tracos
+
+def perguntar_letra():
+    input("escoha uma palvra")
