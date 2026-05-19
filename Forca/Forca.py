bcfdc0dbc63dbdc7768474a8438dd09a1eec61dc
raul <raul.novais@edu.senai.br>
2026-05-19 09:01:25 -0300

    elaboração do jogo da forca

--- Forca/Forca.py
+++ Forca/Forca.py
@@ -111,13 +111,45 @@ def gerar_tracos(palavra: str) -> list:
     return tracos
 
 def perguntar_letra() -> str:
-    resposta=input("escoha uma palvra: ").upper()
+    resposta=input("escoha uma letra: ").upper()
     while len (resposta)!=1:
         resposta = input("eu disse apenas uma letra: ").upper()
         return resposta
-letra = perguntar_letra()
-print(letra)
+# letra = perguntar_letra()
+# print(letra)
 
 def jogar_forca():
     #tela inicial de jogo
-    pass
+    print("""
+********************************************************************
+          *__*_______________________________________*_*
+                     +_+___ Forca ____+_+
+         *__*_______________________________________*_*
+                +_+___ Acerte ou morra____+_+
+         *__*_______________________________________*_*
+         
+********************************************************************
+""")
+
+    input ("aperte enter para apostar a sua vida")
+    limpar()
+
+    #chamar a função escolher_palavra e guardar em uma variavel 
+    palavra_escolida = escolher_palavra()
+
+    #chamar a forca para desenhar a forca 0
+    desenhar_forca(0)
+
+    #chamar a função gerar_palavra e guardar em uma variavel 
+    lista_tracos = gerar_tracos(palavra_escolida)
+
+    #imprimo a lista de traços 
+    print("         ", *lista_tracos)
+
+    #pergutar a letra e guardar em uma variavel 
+    letra_chutada = perguntar_letra() 
+
+    if letra_chutada not in palavra_escolida:
+
+if __name__ == "__main__":
+    jogar_forca()
